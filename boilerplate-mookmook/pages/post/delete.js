89448c5a31415813361fc5b454eaea0c003cdc84
nno3onn <nno3onn@naver.com>
2021-08-18 00:07:24 +0900

    back to previous page

--- boilerplate-mookmook/pages/post/delete.js
+++ boilerplate-mookmook/pages/post/delete.js
@@ -3,6 +3,7 @@ import React from "react";
 import firebase from "firebase";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 class PostDelete extends React.Component {
   constructor(props) {
     super(props);
@@ -10,32 +11,27 @@ class PostDelete extends React.Component {
   }
 
   postDelete = () => {
-    const { docID, type } = this.props;
+    const { postId, type } = this.props;
     const db = firebase.firestore();
 
-    let post = null;
-    type === "movie"
-      ? (post = db.collection("movie"))
-      : (post = db.collection("book"));
+    const post = type === "movie"
+        ? db.collection("movie")
+        : db.collection("book");
 
-    post
-      .doc(docID)
-      .delete()
-      .then(() => {
-        alert("삭제되었습니다.");
-        // 이전 페이지로 돌아가는 코드 추가하기
-      })
-      .catch((error) => {
-        console.error("Error removing document: ", error);
-      });
+    post.doc(postId).delete().then(() => {
+      alert("삭제되었습니다.");
+      history.back();
+    })
+    .catch((error) => {
+      console.error("Error removing document: ", error);
+    });
   };
 
   render() {
     const { postDelete } = this;
     const { postId, type } = this.props;
 
-    return <span><i className="delete bi bi-trash" style={{fontSize: '24px'}} onClick={() => postDelete(postId, type) } /></span>
-
+    return <span className="trash"><i className="trash bi bi-trash" style={{fontSize: '24px'}} onClick={() => postDelete(postId, type) } /></span>
   }
 }
 
