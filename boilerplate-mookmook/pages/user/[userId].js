94f546ffe992043424a5b1e3dc43acf7bde6375e
nno3onn <nno3onn@naver.com>
2021-09-06 17:24:34 +0900

    editing mypage screen

--- boilerplate-mookmook/pages/user/[userId].js
+++ boilerplate-mookmook/pages/user/[userId].js
@@ -38,57 +38,57 @@ const UserPage = () => {
       autoHeightMin="100vh"
       universal={true}
     >
+      <Layout />
       <div className={styles.container}>
-        <Layout />
-        <div className={styles.row}>
-          <div className={styles.side}>
-            <h2>Profile</h2>
-            <a>당신은 상위 몇퍼센트입니다</a>
-            <br />
-            <br /> <br />
-            <br />
-            <div className={styles["post-component"]}>
-              <Image
-                className={styles.image}
-                src={photoURL}
-                width="80"
-                height="80"
-              />
-              <div className={styles.text}>{displayName}</div>
-              <div className={styles.email}>{email}</div>
-            </div>
-            <br />
-            <br /> <br />
-            <br /> <br />
-            <br />
-            <h3>
-              {countMovie}권의 영화를 보고,
-              <br /> {countBook}권의 책을 읽으셨어요
-            </h3>
-            <br />
-            <p>가장 마음에 드는 후기를 하나 선택해주세요</p>
+        {/* <div className={styles.row}> */}
+        <div className={styles.side}>
+          <h2>Profile</h2>
+          <a>당신은 상위 몇퍼센트입니다</a>
+          <br />
+          <br /> <br />
+          <br />
+          <div className={styles["post-component"]}>
+            <Image
+              className={styles.image}
+              src={photoURL}
+              width="80"
+              height="80"
+            />
+            <div className={styles.text}>{displayName}</div>
+            <div className={styles.email}>{email}</div>
           </div>
-          <div className={styles.main}>
-            {" "}
-            {host ? (
-              <DisplayPosts
-                types="all"
-                style={{ zIndex: "-1", paddingLeft: "-300px" }}
-                host={host}
-                color={color}
-                countMovies={countMovies}
-                countBooks={countBooks}
-              />
-            ) : null}
-          </div>
+          <br />
+          <br /> <br />
+          <br /> <br />
+          <br />
+          <h3>
+            {countMovie}권의 영화를 보고,
+            <br /> {countBook}권의 책을 읽으셨어요
+          </h3>
+          <br />
+          <p>가장 마음에 드는 후기를 하나 선택해주세요</p>
         </div>
-        <div className={styles.footer}>
-          <a>
-            mookmook@knu.ac.kr
-            <br />
-            contact: 010-1234-5678
-          </a>
+        <div className={styles.main}>
+          {" "}
+          {host ? (
+            <DisplayPosts
+              types="all"
+              style={{ zIndex: "-1", paddingLeft: "-300px" }}
+              host={host}
+              color={color}
+              countMovies={countMovies}
+              countBooks={countBooks}
+            />
+          ) : null}
         </div>
+      </div>
+      {/* </div> */}
+      <div className={styles.footer}>
+        <a>
+          mookmook@knu.ac.kr
+          <br />
+          contact: 010-1234-5678
+        </a>
       </div>
     </Scrollbars>
   );
