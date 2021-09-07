d2474a573c422cbffc08b4926798cf0a0b6724db
andWHISKEY <tigraphy@gmail.com>
2021-09-07 16:43:22 +0900

    mypage colorpicker

--- boilerplate-mookmook/pages/user/[userId].js
+++ boilerplate-mookmook/pages/user/[userId].js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { Card, Button, Image } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { Scrollbars } from "react-custom-scrollbars";
 import styles from "./styles.module.scss";
 
 import Layout from "../../components/layout";
 import DisplayPosts from "../../components/displayPosts";
 import ColorSelect from "../../components/colorSelect";
-import { Scrollbars } from "react-custom-scrollbars";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -31,74 +31,19 @@ const UserPage = () => {
   const email = account.get("email");
 
   return (
-<<<<<<< Updated upstream
-    <>
-      <Scrollbars
-        autoHide
-        autoHideTimeout={1000}
-        autoHideDuration={200}
-        autoHeight
-        autoHeightMin="100vh"
-        universal={true}
-      >
-        <Layout />
-        <div className={styles.container}>
-          <div className={styles.side}>
-            <h2>Profile</h2>
-            <div className={styles["post-component"]}>
-              <Image
-                className={styles.image}
-                src={photoURL}
-                width="80"
-                height="80"
-              />
-              <p>
-                {displayName}님은 상위{" "}
-                <span style={{ color: "red" }}>0.00 %</span> 입니다
-              </p>
-            </div>
-            <br />
-            <br /> <br />
-            <br /> <br />
-            <br />
-            <h3>
-              {countMovie}개의 영화를 보고,
-              <br /> {countBook}권의 책을 읽으셨어요!
-            </h3>
-            <br />
-            <p>가장 마음에 드는 후기를 하나 선택해주세요</p>
-          </div>
-          <div className={styles.main}>
-            {" "}
-            {host ? (
-              <DisplayPosts
-                types="all"
-                host={host}
-                color={color}
-                countMovies={countMovies}
-                countBooks={countBooks}
-              />
-            ) : null}
-          </div>
-=======
     <Scrollbars
       autoHide
       autoHideTimeout={1000}
       autoHideDuration={200}
       autoHeight
       autoHeightMin="100vh"
-      universal={true}
+      universal
     >
       <Layout />
       <ColorSelect changeColor={changeColor} />
       <div className={styles.container}>
-        {/* <div className={styles.row}> */}
         <div className={styles.side}>
           <h2>Profile</h2>
-          <a>당신은 상위 몇 퍼센트입니다</a>
-          <br />
-          <br /> <br />
-          <br />
           <div className={styles["post-component"]}>
             <Image
               className={styles.image}
@@ -106,28 +51,40 @@ const UserPage = () => {
               width="80"
               height="80"
             />
-            <div className={styles.text}>{displayName}</div>
-            <div className={styles.email}>{email}</div>
+            <p>
+              {displayName}님은 상위{" "}
+              <span style={{ color: "red" }}>0.00 %</span> 입니다
+            </p>
           </div>
           <br />
           <br /> <br />
           <br /> <br />
           <br />
-          <h5>{displayName}님은</h5>
           <h3>
-            {countMovie}권의 영화를 보고,
-            <br /> {countBook}권의 책을 읽으셨어요
+            {countMovie}개의 영화를 보고,
+            <br /> {countBook}권의 책을 읽으셨어요!
           </h3>
           <br />
           <p>가장 마음에 드는 후기를 하나 선택해주세요</p>
->>>>>>> Stashed changes
         </div>
-        <div className={styles.footer}>
-          <p>mookmook@knu.ac.kr</p>
-          <p>contact: 010-1234-5678</p>
+        <div className={styles.main}>
+          {" "}
+          {host ? (
+            <DisplayPosts
+              types="all"
+              host={host}
+              color={color}
+              countMovies={countMovies}
+              countBooks={countBooks}
+            />
+          ) : null}
         </div>
-      </Scrollbars>
-    </>
+      </div>
+      <div className={styles.footer}>
+        <p>mookmook@knu.ac.kr</p>
+        <p>contact: 010-1234-5678</p>
+      </div>
+    </Scrollbars>
   );
 };
 
